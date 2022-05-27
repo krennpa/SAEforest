4cacf84d4467839d1fd02cc3754293f3402a65f0
krennpa <77174075+krennpa@users.noreply.github.com>
2022-05-27 23:48:11 +0200

    added lme4 generics

---
+++ R/useful_generics.R
@@ -0,0 +1,62 @@
+# Extract fixed effects of a SAEforest object --------------------
+# Output correspondes to an object of class ranger
+
+#' @export
+#' @method fixef SAEforest
+
+fixef.SAEforest <- function(object, add.dropped = FALSE, ...) {
+  class_error(object)
+  object$MERFmodel$Forest
+}
+
+# Extract survey data of a SAEforest object ---------------------
+
+#' @export
+#' @method getData SAEforest
+
+getData.SAEforest <- function(object) {
+  class_error(object)
+  object$MERFmodel$data
+}
+
+
+# Extract ranef of a SAEforest object ---------------------------
+
+#' @export
+#' @method ranef SAEforest
+
+ranef.SAEforest <- function(object, ...) {
+  class_error(object)
+  ranef(object$MERFmodel$EffectModel, ...)
+}
+
+# Extract residuals of a SAEforest object -----------------------
+
+#' @export
+#' @method residuals SAEforest
+
+residuals.SAEforest <- function(object, ...) {
+  class_error(object)
+  object$MERFmodel$OOBresiduals
+}
+
+
+# Extract sigma of a SAEforest object ---------------------------
+#' @importFrom stats sigma
+#' @export
+#' @method sigma SAEforest
+
+sigma.SAEforest <- function(object, ...) {
+  class_error(object)
+  sigma(object$MERFmodel$EffectModel, ...)
+}
+
+# Extract VarCorr of a SAEforest object -------------------------
+
+#' @export
+#' @method VarCorr SAEforest
+
+VarCorr.SAEforest <- function(x, sigma = 1, ...) {
+  class_error(x)
+  VarCorr(x$MERFmodel$EffectModel, ...)
+}
